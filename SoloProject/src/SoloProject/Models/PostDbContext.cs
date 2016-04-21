194a4426022c4880997941b3c9fb6e21f03b7544
Taylor <taylorgrohs@gmail.com>
2016-04-20 18:28:25 -0700

    add PostDbContext class.

---
+++ SoloProject/src/SoloProject/Models/PostDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoloProject.Models
+{
+    public class PostDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SoloProject;integrated security = true");
+        }
+    }
+}
