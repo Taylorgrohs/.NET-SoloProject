a9838fca325f10824b65b963e6fc7cb221483087
Taylor <taylorgrohs@gmail.com>
2016-04-20 18:20:36 -0700

    add Post model.

---
+++ SoloProject/src/SoloProject/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoloProject.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        public string Content { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
