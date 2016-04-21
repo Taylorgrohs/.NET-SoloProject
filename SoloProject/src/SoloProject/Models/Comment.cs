60e11ba24bc1a25d4044e61f75638efcf99f5474
Taylor <taylorgrohs@gmail.com>
2016-04-20 18:22:59 -0700

    add Comment model.

---
+++ SoloProject/src/SoloProject/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoloProject.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string Content { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
