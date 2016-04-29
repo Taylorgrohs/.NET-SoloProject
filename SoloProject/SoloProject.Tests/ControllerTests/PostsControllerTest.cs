086b4145ea728d23c1a718ab9f5fa473cb6eecba
Taylor <taylorgrohs@gmail.com>
2016-04-29 13:27:45 -0700

    add modelTests and controllerTests files

---
+++ SoloProject/SoloProject.Tests/ControllerTests/PostsControllerTest.cs
@@ -0,0 +1,26 @@
+﻿//using Microsoft.AspNet.Mvc;
+//using SoloProject.Controllers;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Xunit;
+
+//namespace SoloProject.Tests
+//{
+//    public class PostsControllerTest
+//    {
+//        [Fact]
+//        public void Get_ViewResult_Index_Test()
+//        {
+//            //Arrange
+//            PostsController controller = new PostsController();
+
+//            //Act
+//            var result = controller.Index();
+
+//            //Assert
+//            Assert.IsType<ViewResult>(result);
+//        }
+//    }
+//}
