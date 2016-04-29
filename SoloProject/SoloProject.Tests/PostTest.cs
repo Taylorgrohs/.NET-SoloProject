095b3f555b166e6d6d1daaee7b3686f471767b6d
Taylor <taylorgrohs@gmail.com>
2016-04-29 13:21:46 -0700

    add tests folder to solution.

---
+++ SoloProject/SoloProject.Tests/PostTest.cs
@@ -0,0 +1,37 @@
+﻿using SoloProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SoloProject.Tests
+{
+    public class PostTest
+    { 
+        [Fact]
+        public void GetContentTest()
+        {
+            //Arrange
+            var post = new Post();
+            post.Content = "Wash the dog";
+            //Act
+            var result = post.Content;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+        [Fact]
+        public void GetComment()
+        {
+            var post = new Post();
+            post.Content = "Today";
+            post.PostId = 1;
+            var comment = new Comment();
+            comment.CommentBody = "Test";
+            comment.PostId = 1;
+
+
+        }
+    }
+}
