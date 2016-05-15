0b8dccd595ac3907597ac19904bd9240d291ba11
Taylor <taylorgrohs@gmail.com>
2016-05-15 16:57:41 -0700

    add corrected to/from phone numbers to api call

--- SoloProject/src/SoloProject/Models/Message.cs
+++ SoloProject/src/SoloProject/Models/Message.cs
@@ -16,12 +16,11 @@ namespace SoloProject.Models
 
         public void Send()
         {
-            var client = new RestClient("https://api.twilio.com/2010-04-01");
-            var request = new RestRequest("/Accounts/AC511da8b64e69517082a8911962dfab8c/Messages", Method.POST);
+            var client = new RestClient("https://api.twilio.com/2010-04-01/");
+            var request = new RestRequest("Accounts/AC511da8b64e69517082a8911962dfab8c/Messages", Method.POST);
             request.AddParameter("To", To);
             request.AddParameter("From", From);
             request.AddParameter("Body", Body);
-            request.AddParameter("Status", Status);
             client.Authenticator = new HttpBasicAuthenticator("AC511da8b64e69517082a8911962dfab8c", "b07f4c2a9aaeac2f4d1feee0643dd25b");
             client.Execute(request);
         }
